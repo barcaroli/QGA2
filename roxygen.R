d2d8c0931c1dfa7e92e9153a70a42000f202672b
barcaroli <gbarcaroli@gmail.com>
2022-10-28 19:31:07 +0200

    new version

--- roxygen.R
+++ roxygen.R
@@ -1,1 +1,4 @@
 roxygen2::roxygenize(package.dir = "D:\\Google Drive\\Genetic Algorithm\\QGA\\", roclets = NULL, load_code = NULL, clean = FALSE)
+
+
+roxygen2::roxygenize(package.dir = "C:\\Users\\UTENTE\\Google Drive\\Genetic Algorithm\\QGA\\", roclets = NULL, load_code = NULL, clean = FALSE)
