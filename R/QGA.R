1993dab08e3e85bed392cbe719470c1e8ac26939
barcaroli <gbarcaroli@gmail.com>
2022-10-30 23:49:23 +0100

    last

--- R/QGA.R
+++ R/QGA.R
@@ -295,28 +295,28 @@ QGA <- function(popsize = 20,
                 q_alphabeta[j, 2, i] <- work_q_alphabeta[j, 2, i]
               }
             }
-            if (chromosome[i, j] == 1 & chromosome[best_chromosome[generation], j] == 1) {
-              if (q_alphabeta[j, 1, i]*q_alphabeta[j, 2, i] >= 0) {
-                rot[1, 1] <- cos(theta)
-                rot[1, 2] <- -sin(theta)
-                rot[2, 1] <- sin(theta)
-                rot[2, 2] <- cos(theta)
-                work_q_alphabeta[j, 1, i] <- (rot[1, 1] * q_alphabeta[j, 1, i]) + (rot[1, 2] * q_alphabeta[j, 2, i])
-                work_q_alphabeta[j, 2, i] <- (rot[2, 1] * q_alphabeta[j, 1, i]) + (rot[2, 2] * q_alphabeta[j, 2, i])
-                q_alphabeta[j, 1, i] <- work_q_alphabeta[j, 1, i]
-                q_alphabeta[j, 2, i] <- work_q_alphabeta[j, 2, i]
-              }
-              if (q_alphabeta[j, 1, i]*q_alphabeta[j, 2, i] < 0) {
-                rot[1, 1] <- cos(-theta)
-                rot[1, 2] <- -sin(-theta)
-                rot[2, 1] <- sin(-theta)
-                rot[2, 2] <- cos(-theta)
-                work_q_alphabeta[j, 1, i] <- (rot[1, 1] * q_alphabeta[j, 1, i]) + (rot[1, 2] * q_alphabeta[j, 2, i])
-                work_q_alphabeta[j, 2, i] <- (rot[2, 1] * q_alphabeta[j, 1, i]) + (rot[2, 2] * q_alphabeta[j, 2, i])
-                q_alphabeta[j, 1, i] <- work_q_alphabeta[j, 1, i]
-                q_alphabeta[j, 2, i] <- work_q_alphabeta[j, 2, i]
-              }
-            }
+            # if (chromosome[i, j] == 1 & chromosome[best_chromosome[generation], j] == 1) {
+            #   if (q_alphabeta[j, 1, i]*q_alphabeta[j, 2, i] >= 0) {
+            #     rot[1, 1] <- cos(theta)
+            #     rot[1, 2] <- -sin(theta)
+            #     rot[2, 1] <- sin(theta)
+            #     rot[2, 2] <- cos(theta)
+            #     work_q_alphabeta[j, 1, i] <- (rot[1, 1] * q_alphabeta[j, 1, i]) + (rot[1, 2] * q_alphabeta[j, 2, i])
+            #     work_q_alphabeta[j, 2, i] <- (rot[2, 1] * q_alphabeta[j, 1, i]) + (rot[2, 2] * q_alphabeta[j, 2, i])
+            #     q_alphabeta[j, 1, i] <- work_q_alphabeta[j, 1, i]
+            #     q_alphabeta[j, 2, i] <- work_q_alphabeta[j, 2, i]
+            #   }
+            #   if (q_alphabeta[j, 1, i]*q_alphabeta[j, 2, i] < 0) {
+            #     rot[1, 1] <- cos(-theta)
+            #     rot[1, 2] <- -sin(-theta)
+            #     rot[2, 1] <- sin(-theta)
+            #     rot[2, 2] <- cos(-theta)
+            #     work_q_alphabeta[j, 1, i] <- (rot[1, 1] * q_alphabeta[j, 1, i]) + (rot[1, 2] * q_alphabeta[j, 2, i])
+            #     work_q_alphabeta[j, 2, i] <- (rot[2, 1] * q_alphabeta[j, 1, i]) + (rot[2, 2] * q_alphabeta[j, 2, i])
+            #     q_alphabeta[j, 1, i] <- work_q_alphabeta[j, 1, i]
+            #     q_alphabeta[j, 2, i] <- work_q_alphabeta[j, 2, i]
+            #   }
+            # }
           }
         }
       }
