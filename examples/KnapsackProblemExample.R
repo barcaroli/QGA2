e878592b4ba180a73ea1756c06cb65d8215abb61
Giulio Barcaroli <gbarcaroli@gmail.com>
2022-11-02 20:10:07 +0100

    knapsack

--- examples/KnapsackProblemExample.R
+++ examples/KnapsackProblemExample.R
@@ -13,13 +13,13 @@ set.seed(1234)
 items$weight <- rnorm(300,mean=50,sd=20)
 hist(items$weight)
 sum(items$weight)
-maxweight = sum(items$weight) / 3
+maxweight = sum(items$weight) / 2
 maxweight
 
 #----------------------
 # Set parameters
 popsize = 20
-generation_max = 30
+generation_max = 250
 nvalues_sol = 2
 Genome = nrow(items)
 thetainit = 3.1415926535 * 0.05
@@ -33,7 +33,7 @@ eval_fitness = KnapsackProblem
 eval_func_inputs = list(items,
                         maxweight)
 plotting = TRUE
-verbose = TRUE
+verbose = FALSE
 
 #----------------------
 # Perform optimization
@@ -58,4 +58,35 @@ solution <- QGA(popsize,
 solution <- solution - 1
 sum(solution)
 sum(items$weight[solution])
+maxweight
+
+#-----------------------------------------
+# Compare with classical genetic algorithm
+library(genalg)
+evaluate <- function(solution) {
+  tot_items <- sum(solution)
+  # Penalization
+  if (sum(items$weight[solution]) > maxweight) {
+    tot_items <- tot_items - (sum(items$weight[solution]) - maxweight)  
+  }
+  return(-tot_items)
+}
+rbga.results <- rbga.bin(size=nrow(items),
+         suggestions=NULL,
+         popSize=20, 
+         iters=1000, 
+         elitism=NA, 
+         zeroToOneRatio=round(maxweight*10/sum(items$weight)),
+         evalFunc=evaluate)
+plot(rbga.results)
+filter = rbga.results$evaluations == min(rbga.results$evaluations)
+bestObjectCount = sum(rep(1, rbga.results$popSize)[filter])
+if (bestObjectCount > 1) {
+  bestSolution = rbga.results$population[filter, ][1, 
+  ]
+} else {
+  bestSolution = rbga.results$population[filter, ]
+}
+sum(bestSolution)
+sum(items$weight[bestSolution])
 maxweight
