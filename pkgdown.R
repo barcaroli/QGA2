62802bbd598eaa9aff666aab9b51d871813861cd
Giulio Barcaroli <gbarcaroli@gmail.com>
2024-05-28 17:57:13 +0200

    pkgdown

---
+++ pkgdown.R
@@ -0,0 +1,24 @@
+# require(devtools)
+# use_readme_rmd()
+# use_news_md()
+# use_vignette("SamplingStrata")
+# use_github_links()
+# # use_travis()
+# use_cran_badge()
+
+# install.packages("tvthemes") # v1.1.0
+# library(tvthemes)
+
+
+# devtools::install_github("hadley/pkgdown")
+# setwd("C:/Users/UTENTE/Google Drive/Genetic Algorithm/QGA")
+setwd("D:\\Google Drive\\Genetic Algorithm\\QGA")
+
+library(pkgdown)
+# usethis::use_pkgdown()
+# build_favicon(pkg = ".")
+# init_site(pkg = ".")
+build_site()
+
+
+
