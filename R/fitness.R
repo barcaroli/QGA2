5facf4f501527f50277d31c8ce0edbabe7fda7ca
Giulio Barcaroli <gbarcaroli@gmail.com>
2025-05-31 13:12:21 +0200

    Update fitness.R

--- R/fitness.R
+++ R/fitness.R
@@ -9,22 +9,25 @@ evaluate <- function(chromosome,
                      nvalues_sol,
                      generation,
                      eval_fitness,
-                     eval_func_inputs){
-  fitness <- array(0.0, c(1, popsize))
+                     eval_func_inputs) {
+  # Pre-allocate fitness vector
+  fitness <- numeric(popsize)
   fitness_total <- 0
-  fitness_average <- -99999999
-  fitness_max <- -999999999
+  fitness_max <- -Inf
   the_best_chromosome <- 0
-  for (i in c(1:popsize)) {
-    solution1 <- array(chromosome[i,],c(geneLength,Genome))
-    solution <- c(rep(0,Genome))
-    for (x in c(1:Genome)) {
-      for (y in c(1:geneLength)) {
-        solution[x] <- solution[x] + solution1[y,x]*2^(geneLength - y) 
-      }
-    }
-    solution <- solution + 1
-    fitness[i] <- eval_fitness(solution,eval_func_inputs)
+  solution_max <- NULL  # Ensure it's defined even if not used
+
+  # Precompute binary weights for efficiency
+  bin_weights <- 2^((geneLength - 1):0)
+  
+  # Vectorized evaluation of all solutions
+  for (i in seq_len(popsize)) {
+    # Reshape gene row into matrix
+    solution1 <- matrix(chromosome[i, ], nrow = geneLength, ncol = Genome)
+    # Vectorized binary to integer conversion and add 1
+    solution <- colSums(solution1 * bin_weights) + 1
+    # Evaluate fitness
+    fitness[i] <- eval_fitness(solution, eval_func_inputs)
     fitness_total <- fitness_total + fitness[i]
     if (fitness[i] >= fitness_max) {
       fitness_max <- fitness[i]
@@ -32,6 +35,7 @@ evaluate <- function(chromosome,
       solution_max <- chromosome[i, ]
     }
   }
+
   fitness_average <- fitness_total / popsize
   best_chromosome[generation] <- the_best_chromosome
   return(list(
@@ -39,6 +43,6 @@ evaluate <- function(chromosome,
     fitness_max = fitness_max,
     fitness_average = fitness_average,
     best_chromosome = best_chromosome,
-    solution_max = solution_max)
-  )
+    solution_max = solution_max
+  ))
 }
